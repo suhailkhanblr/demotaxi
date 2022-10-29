5e6748cf0e7334de7e38509cb7ada9357012f520
suhailkhanblr <73229497+suhailkhanblr@users.noreply.github.com>
2022-10-29 22:30:47 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
         },
         child: MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'product name',
+            title: 'Demo Taxi',
             theme: ThemeData(),
             home: const LoadingPage()));
   }
